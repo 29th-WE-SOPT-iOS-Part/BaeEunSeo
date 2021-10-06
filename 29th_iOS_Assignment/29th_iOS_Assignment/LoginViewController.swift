8d0dccc74035da05c0853ce527ac237590b6e758
eunseo5355 <eunseo5355@naver.com>
2021-10-06 19:27:29 +0900

    ✨[Feat] #1 로그인, 회원가입, 확인 UI 구현

--- 29th_iOS_Assignment/29th_iOS_Assignment/LoginViewController.swift
+++ 29th_iOS_Assignment/29th_iOS_Assignment/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  29th_iOS_Assignment
 //
 //  Created by 배은서 on 2021/10/06.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
---
+++ 29th_iOS_Assignment/29th_iOS_Assignment/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  29th_iOS_Assignment
+//
+//  Created by 배은서 on 2021/10/06.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
